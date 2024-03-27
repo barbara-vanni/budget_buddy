2840438c8340cf3a06461224ce49fa761df037e0
Dhjali <barbara.vanni@laplateforme.io>
2024-03-27 17:15:58 +0100

    [MODIFY] emain into mail

--- Model/UserRepository.py
+++ Model/UserRepository.py
@@ -1,16 +1,16 @@
-from RequestDb import RequestDb
+from Model.RequestDb import RequestDb
 import hashlib
 
 class UserRepository(RequestDb):
     def __init__(self):
         super().__init__()
 
-    def create_user(self, name, firstname, email, password):
+    def create_user(self, name, firstname, mail, password):
         password_bytes = password.encode('utf-8')
         hash_password = hashlib.sha256()
         hash_password.update(password_bytes)
         hashed_password = hash_password.hexdigest()
-        self.create('user', {'name': name, 'firstname': firstname, 'email': email, 'password': hashed_password})
+        self.create('user', {'name': name, 'firstname': firstname, 'mail': mail, 'password': hashed_password})
 
     def read_user(self, conditions=None):
         return self.read('user', conditions)
@@ -26,13 +26,13 @@ class UserRepository(RequestDb):
     def delete_user(self, conditions):
         self.delete('user', conditions)
 
-user_repo = UserRepository()
-# user_repo.create_user(name='Jean', firstname='Jacques', email='jj@gmail.com', password='pass147258+')
-user_repo.read_user()
-print(user_repo.read_user())
-user_repo.update_user({'name': 'Jean', 'firstname': 'Jacques', 'email': 'jb@gmail.com', 'password': '7258+'})
-print(user_repo.read_user())
-user_repo.delete_user('email = "jb@gmail.com"')
-print(user_repo.read_user())
+# user_repo = UserRepository()
+# # user_repo.create_user(name='Jean', firstname='Jacques', email='jj@gmail.com', password='pass147258+')
+# user_repo.read_user()
+# print(user_repo.read_user())
+# user_repo.update_user({'name': 'Jean', 'firstname': 'Jacques', 'email': 'jb@gmail.com', 'password': '7258+'})
+# print(user_repo.read_user())
+# user_repo.delete_user('email = "jb@gmail.com"')
+# print(user_repo.read_user())
 
 
