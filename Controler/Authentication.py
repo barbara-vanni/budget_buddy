77ce0cde097878aa51b0d0af598e6b14d0f5137c
Dhjali <barbara.vanni@laplateforme.io>
2024-03-27 16:34:29 +0100

    [MODIFY] email to mail in Authentication.py

--- Controler/Authentication.py
+++ Controler/Authentication.py
@@ -11,10 +11,11 @@ class Authentication:
         self.user_repo = UserRepository()
 
     def authenticate(self, mail, password):
-        '''method for authentication: check the mail input
+        '''
+        method for authentication: check the mail input
         if user exist, check the input password 
         '''
-        user_data = self.user_repo.read_user(conditions=f"email = '{mail}'")
+        user_data = self.user_repo.read_user(conditions=f"mail = '{mail}'")
         # if the user exist in the database
         if user_data:
             # collect the hashed password from the Db
@@ -29,19 +30,24 @@ class Authentication:
             # Compare the hashed password of the db and the one we just hash
             if hashed_password_input == hashed_password_db:
                 return True
-        return False
-    
+        return False   
 
-    def password_enter(password):
+    def password_enter(self, password):
+        '''
+        method for password verification: check if the password contains at least 8 characters, 1 uppercase, 1 lowercase, 1 digit and 1 special character
+        '''
         autorized_Special_Char = "!@#$%^&*"
         if len(password) < 8 or not any(char.isupper() for char in password) or not any(char.islower() for char in password) or not any(char in autorized_Special_Char for char in password) or not any(char.isdigit() for char in password):
             return False
         return True
     
-    def create_account(self, name, firstname, email, password):
+    def create_account(self, name, firstname, mail, password):
+        '''
+        method for account creation: check the password input first 
+        '''
         check_password = self.password_enter(password)
         if check_password == True :
-            self.user_repo.create_user(name, firstname, email, password)
-            User(name, firstname, email, password)
+            self.user_repo.create_user(name, firstname, mail, password)
+            User(name, firstname, mail, password)
         else :
             print('Enter a valid password')
