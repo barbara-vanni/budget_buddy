6db05aa0b26d80614608b7a2bf23ba57476c753c
Dhjali <barbara.vanni@laplateforme.io>
2024-03-29 10:32:50 +0100

    [ADD] Screen class for the opening of the windows

---
+++ View/Screen.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+
+class Screen():
+    def __init__(self) -> None:
+        self.__screen = tk.Tk()
+        self.__width = 900
+        self.__height = 600
+        self.__canvas = tk.Canvas(self.__screen, width=self.__width, height=self.__height)
+        self.__window_canvas = None
+
+
+    def get_screen(self):
+        return self.__screen
+    
+    def get_canvas(self):
+        return self.__canvas
+
+    def get_window_canvas(self):
+        return self.__window_canvas
+    
+    # def draw_canvas(self):
+    #     # self.__screen.title(f"Budget {fisrt_name}, {last_name}")
+    #     self.__canvas = self.__canvas
+    #     self.__canvas.pack()
+    #     return self.__canvas
+    
+    def draw_window_canvas(self):
+        self.__window_canvas = tk.Canvas(self.__screen, width = self.__width - 150, height = self.__height - 150)
+        self.__window_canvas.pack()
