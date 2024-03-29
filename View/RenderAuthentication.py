2a7e168fe2f013e83f431b1a3833739bcb229f91
Dhjali <barbara.vanni@laplateforme.io>
2024-03-29 15:26:58 +0100

    [ADD] check_sign method for the sign in

--- View/RenderAuthentication.py
+++ View/RenderAuthentication.py
@@ -2,6 +2,7 @@ from View.Entry import CustomEntry
 from View.Image import Image
 from View.Button import Button
 from View.Screen import Screen
+from Controler.Authentication import Authentication
 
 
 class RenderAuthentication:
@@ -9,6 +10,7 @@ class RenderAuthentication:
         self.screen_object = Screen()
         self.canvas = self.screen_object.get_canvas()
         self.custom_entries = []
+        self.authentication = Authentication()
 
     def get_screen_object(self):
         return self.screen_object
@@ -20,7 +22,6 @@ class RenderAuthentication:
 
         for entry in self.custom_entries:
             entry.destroy_entry()
-
 
         background_image = Image(self.canvas, 0, 0, './assets/bcg_menu.png')
         self.draw_canvas()
@@ -35,9 +36,19 @@ class RenderAuthentication:
         self.screen_object.get_screen().mainloop()
         self.canvas.update()
 
-    # def on_real_sign_in_button_click(self, auth, entry1, entry2, entry3, entry4):
-    #     auth.create_account(entry1.get_value(), entry2.get_value(), entry3.get_value(), entry4.get_value())
-    #     self.render_log_in()
+
+    def check_sign_in(self, entry1, entry2, entry3, entry4):
+        name = entry1.get_value()
+        username = entry2.get_value()
+        email = entry3.get_value()
+        password = entry4.get_value()
+
+        print("check_auth", name, username, email, password)
+        if self.authentication.create_account(name, username, email, password):
+            self.render_log_in()
+            print("Account created")
+        else:
+            print("Password not valid")            
 
     def render_sign_in(self):
 
@@ -52,24 +63,16 @@ class RenderAuthentication:
         entry3 = CustomEntry(self.canvas, "Email", x=300, y=293)
         entry4 = CustomEntry(self.canvas, "Password", x=300, y=369, show='*')
 
+
         self.custom_entries.extend([entry1, entry2, entry3, entry4])
 
         real_sign_in_button = Button(self.canvas, 330, 450, './assets/sign_in_button_page.png', None)
-        # real_sign_in_button.bind('<Button-1>', lambda event: self.on_real_sign_in_button_click(entry1, entry2, entry3, entry4))
+        real_sign_in_button.bind('<Button-1>', lambda event: self.check_sign_in(entry1, entry2, entry3, entry4))
         
         self.screen_object.get_screen().mainloop()
         self.canvas.update()
+
         
-    # def check_authenticate(self, mail, password):
-    #     return_authenticate = auth.authenticate(mail, password)
-    #     if return_authenticate[0] == True:
-    #         user = return_authenticate[1]
-            
-    #         client.connect_to_server('10.10.107.118', 8080)
-    #         threading.Thread(target=read_messages_loop).start()
-    #         render_chat(user)
-    #     else:
-    #         print("Authentication failed")
 
     def render_log_in(self):
 
@@ -80,12 +83,12 @@ class RenderAuthentication:
         background_image.draw()
 
         entry5 = CustomEntry(self.canvas, "Email", x=300, y=195)
-        entry6 = CustomEntry(self.canvas, "Password", x=300, y=283)
+        entry6 = CustomEntry(self.canvas, "Password", x=300, y=283, show='*')
 
         self.custom_entries.extend([entry5, entry6])
 
         real_log_in_button = Button(self.canvas, 340, 360, './assets/log_in_button_page.png', None)
-        # real_log_in_button.bind('<Button-1>', lambda event: self.check_authenticate(entry5.get_value(), entry6.get_value()))
+        real_log_in_button.bind('<Button-1>', lambda event: self.check_authenticate(entry5.get_value(), entry6.get_value()))
 
         new_here_button = Button(self.canvas, 485, 452, './assets/sign_in_button_log_page.png', None)
         new_here_button.bind('<Button-1>', lambda event : self.render_sign_in())
