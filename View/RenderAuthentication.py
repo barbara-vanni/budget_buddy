db8a9570fa786d63bf892c47b53709fd1b1c4427
Dhjali <barbara.vanni@laplateforme.io>
2024-03-29 12:00:30 +0100

    [ADD] method for the sign_in render_menu

--- View/RenderAuthentication.py
+++ View/RenderAuthentication.py
@@ -3,12 +3,12 @@ from View.Image import Image
 from View.Button import Button
 from View.Screen import Screen
 
-custom_entries = []
 
 class RenderAuthentication:
     def __init__(self):
         self.screen_object = Screen()
         self.canvas = self.screen_object.get_canvas()
+        self.custom_entries = []
 
     def get_screen_object(self):
         return self.screen_object
@@ -18,15 +18,16 @@ class RenderAuthentication:
     
     def render_main_menu(self):
 
-        for entry in custom_entries:
+        for entry in self.custom_entries:
             entry.destroy_entry()
+
 
         background_image = Image(self.canvas, 0, 0, './assets/bcg_menu.png')
         self.draw_canvas()
         background_image.draw()
 
         sign_in_button = Button(self.canvas, 50, 500, './assets/sign_in_button.png', None)
-        # sign_in_button.bind('<Button-1>', lambda event: self.render_sign_in(self.screen_object, canvas))
+        sign_in_button.bind('<Button-1>', lambda event: self.render_sign_in())
 
         log_in_button = Button(self.canvas, 580, 500, './assets/log_in_button.png', None)
         log_in_button.bind('<Button-1>', lambda event: self.render_log_in())
@@ -39,7 +40,11 @@ class RenderAuthentication:
     #     self.render_log_in()
 
     def render_sign_in(self):
-        background_image = Image(self.canvas, 0, 0, './assets/bcg_signin.png')
+
+        for entry in self.custom_entries:
+            entry.destroy_entry()
+
+        background_image = Image(self.canvas, 0, 0, './assets/bcg_sign_menu.png')
         background_image.draw()
 
         entry1 = CustomEntry(self.canvas, "Name", x=300, y=141)
@@ -47,10 +52,10 @@ class RenderAuthentication:
         entry3 = CustomEntry(self.canvas, "Email", x=300, y=293)
         entry4 = CustomEntry(self.canvas, "Password", x=300, y=369, show='*')
 
-        custom_entries.extend([entry1, entry2, entry3, entry4])
+        self.custom_entries.extend([entry1, entry2, entry3, entry4])
 
-        real_sign_in_button = Button(self.canvas, 330, 450, './assets/sign_in_button_2.png', None)
-        real_sign_in_button.bind('<Button-1>', lambda event: self.on_real_sign_in_button_click(entry1, entry2, entry3, entry4))
+        real_sign_in_button = Button(self.canvas, 330, 450, './assets/sign_in_button_page.png', None)
+        # real_sign_in_button.bind('<Button-1>', lambda event: self.on_real_sign_in_button_click(entry1, entry2, entry3, entry4))
         
         self.screen_object.get_screen().mainloop()
         self.canvas.update()
@@ -68,7 +73,7 @@ class RenderAuthentication:
 
     def render_log_in(self):
 
-        for entry in custom_entries:
+        for entry in self.custom_entries:
             entry.destroy_entry()
 
         background_image = Image(self.canvas, 0, 0, './assets/bcg_log_menu.png')
@@ -77,13 +82,13 @@ class RenderAuthentication:
         entry5 = CustomEntry(self.canvas, "Email", x=300, y=195)
         entry6 = CustomEntry(self.canvas, "Password", x=300, y=283)
 
-        custom_entries.extend([entry5, entry6])
+        self.custom_entries.extend([entry5, entry6])
 
         real_log_in_button = Button(self.canvas, 340, 360, './assets/log_in_button_page.png', None)
         # real_log_in_button.bind('<Button-1>', lambda event: self.check_authenticate(entry5.get_value(), entry6.get_value()))
 
         new_here_button = Button(self.canvas, 485, 452, './assets/sign_in_button_log_page.png', None)
-        # new_here_button.bind('<Button-1>', self.render_sign_in(self.screen, self.canvas))
+        new_here_button.bind('<Button-1>', lambda event : self.render_sign_in())
 
         self.screen_object.get_screen().mainloop()
         self.canvas.update()
