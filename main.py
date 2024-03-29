9fe1e77b5ba2f6291be9f8fac0eba3feaaa8a73d
Dhjali <barbara.vanni@laplateforme.io>
2024-03-29 10:24:23 +0100

    [MODIFY] call the state function from menu_current_render

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
-from Controler.Authentication import Authentication
+
+# from View.RenderAuthentication import RenderAuthentication
+from View.menu_current_render import *
 # from Controler.Budget import Budget
 
 try : 
     running = True
 
+    
+
     while running :
-        print('1. Login')
-        print('2. Create an account')
-        print('3. Quit')
-        auth = Authentication()
-        choice = input('Enter your choice: ')
-        if choice == '1':
-            mail = input('Enter your mail: ')
-            password = input('Enter your password: ')
-            print ('mail :', mail, 'password :', password )
-            if auth.authenticate(mail, password):
-                print('You are connected')
-            else:
-                print('Wrong mail or password')
-        elif choice == '2':
-            name = input('Enter your name: ')
-            firstname = input('Enter your firstname: ')
-            mail = input('Enter your mail: ')
-            password = input('Enter your password: ')
-            auth.create_account(name, firstname, mail, password)
-        elif choice == '3':
-            print('Goodbye')
-            running = False
-        else:
-            print('Enter a valid choice')
+        state()()
+    
+        # print('1. Login')
+        # print('2. Create an account')
+        # print('3. Quit')
+        # auth = Authentication()
+        # choice = input('Enter your choice: ')
+        # if choice == '1':
+        #     mail = input('Enter your mail: ')
+        #     password = input('Enter your password: ')
+        #     print ('mail :', mail, 'password :', password )
+        #     if auth.authenticate(mail, password):
+        #         print('You are connected')
+        #     else:
+        #         print('Wrong mail or password')
+        # elif choice == '2':
+        #     name = input('Enter your name: ')
+        #     firstname = input('Enter your firstname: ')
+        #     mail = input('Enter your mail: ')
+        #     password = input('Enter your password: ')
+        #     auth.create_account(name, firstname, mail, password)
+        # elif choice == '3':
+        #     print('Goodbye')
+        #     running = False
+        # else:
+        #     print('Enter a valid choice')
 
 except Exception as e:
     print(f"Error: {e}")
