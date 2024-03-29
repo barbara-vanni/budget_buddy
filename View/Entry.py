1242bebc9d348ef5131186e76fba1e883ba33084
Dhjali <barbara.vanni@laplateforme.io>
2024-03-29 11:27:50 +0100

    [MODIFY] change the color

--- View/Entry.py
+++ View/Entry.py
@@ -4,10 +4,10 @@ from tkinter import Entry, StringVar
 class CustomEntry:
     def __init__(self, parent, default_text, x, y):
         self.default_text = default_text
-        self.frame = tk.Frame(parent, bg='black', bd=0, padx=0, pady=0, relief="flat")
+        self.frame = tk.Frame(parent, bg='white', bd=0, padx=0, pady=0, relief="flat")
         self.frame.place(x=x, y=y)
         self.entry_var = StringVar()
-        self.entry = Entry(self.frame, textvariable=self.entry_var, width=20, font=("Arial", 20), insertbackground="red", bg="black", fg="white", relief="flat")
+        self.entry = Entry(self.frame, textvariable=self.entry_var, width=20, font=("Arial", 20), insertbackground="red", bg="white", fg="black", relief="flat")
         self.entry.insert(0, default_text)
         self.entry.bind('<FocusIn>', self.on_entry_click)
         self.entry.pack()
@@ -15,7 +15,7 @@ class CustomEntry:
     def on_entry_click(self, event):
         if self.entry.get() == self.default_text:
             self.entry.delete(0, tk.END)
-            self.entry.config(fg='white') 
+            self.entry.config(fg='black') 
 
     def get_value(self):
         return self.entry_var.get()
