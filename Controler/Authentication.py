73881c9b83c3caf5b74bd1edb64d17d9039cbfd6
Dhjali <barbara.vanni@laplateforme.io>
2024-04-01 02:45:19 +0200

    [MODIFY] return of the authenticate method

--- Controler/Authentication.py
+++ Controler/Authentication.py
@@ -35,7 +35,7 @@ class Authentication:
             # Compare the hashed password of the db and the one we just hash
             if hashed_password_input == hashed_password_db:
                 return User(user_data[0][1], user_data[0][2], user_data[0][3], user_data[0][4])
-        return False   
+        return True   
 
 
     def password_enter(self, password):
